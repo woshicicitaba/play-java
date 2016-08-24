f264273d010d662c9ebe24fa730f1ca990a9d178
wh <1@1.com>
2016-08-24 22:01:20 +0800

    1

--- app/controllers/GetData.java
+++ app/controllers/GetData.java
@@ -22,7 +22,7 @@ import java.util.logging.Logger;
 public class GetData extends Controller {
     public play.mvc.Result getDta() throws IOException {
         int i = 1;
-        int PicName=1;
+        int PicName = 1;
         for (int j = 1; j <= 300; j++) {
             String url = "http://www.qiushimm.com/page/" + i;
             i++;
@@ -35,7 +35,7 @@ public class GetData extends Controller {
             for (org.jsoup.nodes.Element e : elements) {
                 String ImagAlt = String.valueOf(e.attr("src"));
                 ImagAlt = ImagAlt.substring(ImagAlt.lastIndexOf("."));
-                ImagAlt="A"+i+"B"+PicName+ImagAlt;
+                ImagAlt = "A" + i + "B" + PicName + ImagAlt;
                 downLoadFromUrl(e.attr("src"), ImagAlt, "E:/im");
                 PicName++;
                 // play.Logger.info(String.valueOf(ImagAlt));
@@ -72,7 +72,7 @@ public class GetData extends Controller {
         if (inputStream != null) {
             inputStream.close();
         }
-     //   play.Logger.info("info:" + url + " download success");
+        //   play.Logger.info("info:" + url + " download success");
     }
 
     public static byte[] readInputStream(InputStream inputStream) throws IOException {
@@ -84,5 +84,22 @@ public class GetData extends Controller {
         }
         bos.close();
         return bos.toByteArray();
+    }
+
+
+    public play.mvc.Result getWord() throws IOException {
+
+        String url = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=it%E7%AC%91%E8%AF%9D&rsv_pq=e60f799b00048810&rsv_t=afdbamZDoTqF7H8%2BynXUOkihOP2agjYnE3P5xxujfo0B02ob2EqQVsRLwU8&rqlang=cn&rsv_enter=1&rsv_sug3=10&rsv_sug1=6&rsv_sug7=100";
+        Document doc = Jsoup.connect(url).header("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2").get();
+        org.jsoup.select.Elements elements = doc.select("a.c-showurl");
+
+        //play.Logger.info(String.valueOf(elements));
+
+        for (org.jsoup.nodes.Element e : elements) {
+            String abs=e.attr("abs:href");
+            play.Logger.info(String.valueOf(abs));
+        }
+
+        return ok();
     }
 }
