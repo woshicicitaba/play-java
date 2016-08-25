268eb0565b8aef50cb8a5a7923625cb7cd40b44a
wh <1@1.com>
2016-08-25 15:07:41 +0800

    1

--- app/models/dataBase.java
+++ app/models/dataBase.java
@@ -4,6 +4,7 @@ import com.avaje.ebean.Model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
@@ -33,6 +34,7 @@ public class dataBase extends Model {
 
     public static Finder<Long, dataBase> find = new Finder<>(dataBase.class);
 
+    @Id
     @Column(name = "ID")
     public Long getId() {
         return id;
