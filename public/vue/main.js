bf43b51617dd3412384cfc2db63e4884c2d6cfc2
wh <1@1.com>
2017-03-06 11:11:59 +0800

    1

--- public/vue/main.js
+++ public/vue/main.js
@@ -1,40 +1,64 @@
 /**
  * Created by admin on 2017-2-24.
  */
-var app_main = new Vue({
+var app = new Vue({
     el: '#app',
-    data: {data_main:{}},
-    //methods: {
-    //    add: function () {
-    //        var r;
-    //        var jsonData = {};
-    //        var order = 1;
-    //        var num = 10;
-    //
-    //        jsonData.order = order;
-    //        jsonData.num = num;
-    //        $.post('/get_Old_Data', jsonData, function (res) {
-    //            // process response
-    //            r = $.parseJSON(res);
-    //            app_main.data_main = r.data;
-    //        })
-    //    }
-    //}
+    data: {
+        data_main: []
+    },
+    methods: {
+        add: function (i) {
+            if (app.data_main[i].show_commit == false && app.data_main[i].flag_first == false) {
+                app.data_main[i].show_commit = true;
+                app.data_main[i].flag_first = false;
+                //console.info(app.data_main[i].id);
+                //发送请求获取评论
+                var jsonComment = {};
+                jsonComment.id = app.data_main[i].id;
+                $.post('/get_Old_Comment', jsonComment, function (res) {
+                    var rr = $.parseJSON(res);
+                    app.data_main[i].commit = rr.pic_comment1;
+                })//post
+            }//if
 
+            else if (app.data_main[i].show_commit == false && app.data_main[i].flag_first == true) {
+                app.data_main[i].show_commit = true;
+            }
+            else {
+                app.data_main[i].show_commit = false;
+            }
+        },
+
+        heart: function (i) {
+            if (app.data_main[i].class_heart == false) {
+                app.data_main[i].class_heart = true;
+            }
+            else {
+                app.data_main[i].class_heart = false;
+            }
+        }
+
+    }
 })
 
-var r;
-function add() {
+function load() {
 
     var jsonData = {};
     var order = 1;
     var num = 10;
+    var r;
 
     jsonData.order = order;
     jsonData.num = num;
     $.post('/get_Old_Data', jsonData, function (res) {
         // process response
         r = $.parseJSON(res);
-        app_main.data_main = r.data;
+        for (var i = 0; i < r.data.length; i++) {
+            r.data[i].show_commit = false;
+            r.data[i].class_heart = false;
+            r.data[i].flag_first = false;
+            r.data[i].commit = [];
+            app.data_main.push(r.data[i]);
+        }
     })
 }
