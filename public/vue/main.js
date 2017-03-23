444d144ba964921f5b54a91d1eac7805a6fa3530
wh <1@1.com>
2017-03-23 14:06:40 +0800

    1

--- public/vue/main.js
+++ public/vue/main.js
@@ -1,18 +1,29 @@
 /**
  * Created by admin on 2017-2-24.
  */
+
+var order = 1;
+var num = 10;
+var add_flag = true;
+
 var app = new Vue({
     el: '#app',
     data: {
-        data_main: []
+        data_main: [],
+        button_flag_open: false,
+        button_flag_dialogue: false,
+        message: ''
+    },
+    ready: function () {
+        //è¿™é‡Œæ˜¯vueåˆå§‹åŒ–å®Œæˆåæ‰§è¡Œçš„å‡½æ•°
+        window.addEventListener('scroll', this.handleScroll);
     },
     methods: {
-        add: function (i) {
+        add_commit: function (i) {
             if (app.data_main[i].show_commit == false && app.data_main[i].flag_first == false) {
                 app.data_main[i].show_commit = true;
                 app.data_main[i].flag_first = false;
                 //console.info(app.data_main[i].id);
-                //·¢ËÍÇëÇó»ñÈ¡ÆÀÂÛ
                 var jsonComment = {};
                 jsonComment.id = app.data_main[i].id;
                 $.post('/get_Old_Comment', jsonComment, function (res) {
@@ -30,22 +41,100 @@ var app = new Vue({
         },
 
         heart: function (i) {
+
             if (app.data_main[i].class_heart == false) {
                 app.data_main[i].class_heart = true;
             }
             else {
                 app.data_main[i].class_heart = false;
             }
-        }
+        },
 
+        add_pic: function () {
+            var jsonData = {};
+            var r;
+            order = order + 10;
+            jsonData.order = order;
+            jsonData.num = num;
+            $.post('/get_Old_Data', jsonData, function (res) {
+                // process response
+                r = $.parseJSON(res);
+                for (var i = 0; i < r.data.length; i++) {
+                    r.data[i].show_commit = false;
+                    r.data[i].class_heart = false;
+                    r.data[i].flag_first = false;
+                    r.data[i].commit = [];
+                    app.data_main.push(r.data[i]);
+                }
+                setTimeout(function () {
+                    add_flag = true;
+                }, 3000);
+            })
+        },
+
+        show_detail: function () {
+            //console.info(this.button_flag_open);
+            if (this.button_flag_open == false) {
+                this.button_flag_open = true;
+            }
+            else {
+                this.button_flag_open = false;
+            }
+        },
+
+        show_dialogue: function () {
+            //console.info(this.button_flag_open);
+            if (this.button_flag_dialogue == false) {
+                this.button_flag_dialogue = true;
+            }
+            else {
+                this.button_flag_dialogue = false;
+            }
+        },
+
+        show_commit_area: function (i) {
+            app.data_main[i].submit_commit = true;
+        },
+
+        submit_commit: function (index) {
+            //alert(this.message);
+            //alert(app.data_main[index].id);
+            var jsonData = {};
+            var r;
+            jsonData.id = app.data_main[index].id;
+            jsonData.value = this.message;
+            $.post('/submit_comment', jsonData, function (res) {
+                // process response
+                r = $.parseJSON(res);
+                //r.pic_comment1[0].floor = 'æ–°è¿›X';
+                app.data_main[index].commit.unshift(r.pic_comment1[0]);
+                //console.info(r.pic_comment1[0]);
+            });
+            this.message = null;
+        },
+
+        handleScroll: function () {
+            var scrollTop = $(window).scrollTop();
+            var scrollHeight = $(document).height();
+            var windowHeight = $(window).height();
+            //console.info(scrollTop + '$$' + scrollHeight + '$$' + windowHeight);
+            //console.info((scrollTop + windowHeight + 30) == (scrollHeight));
+            //console.info((scrollTop + windowHeight + 30));
+            //console.info((scrollHeight));
+            console.info(add_flag);
+            if (((scrollTop + windowHeight + 50) > (scrollHeight)) && (add_flag == true)) {
+                //alert('a');
+                //console.info((scrollTop + windowHeight+30) + '$$' + (scrollHeight) + '$$' +(scrollTop + windowHeight+30-scrollHeight) )
+                add_flag = false;
+                this.add_pic();
+            }
+        }
     }
 })
 
 function load() {
 
     var jsonData = {};
-    var order = 1;
-    var num = 10;
     var r;
 
     jsonData.order = order;
@@ -58,6 +147,7 @@ function load() {
             r.data[i].class_heart = false;
             r.data[i].flag_first = false;
             r.data[i].commit = [];
+            r.data[i].submit_commit = false;
             app.data_main.push(r.data[i]);
         }
     })
